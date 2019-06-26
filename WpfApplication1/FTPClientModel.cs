70299878b3839c6620eb164e148ace943e2d31f1
Ben <zenbenjam@gmail.com>
2019-06-25 19:16:21 -0700

    Added model/viewmodel

--- WpfApplication1/FTPClientModel.cs
+++ WpfApplication1/FTPClientModel.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
 
 namespace WpfApplication1
 {
-    class FTPClientViewModel
+    class FTPClientModel
     {
     }
 }

efd77589c9dc1f170c9ceaf463c857196e70122b
Ryan Albazzaz <haider3@pdx.edu>
2019-07-09 18:23:53 -0700

    Added the removeDir and removeFile to the project that were coded by david djernaes

---
+++ WpfApplication1/removeDir.cs
@@ -0,0 +1,74 @@
+﻿// David Djernaes
+// CS410 Agile group project
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+using System.IO.IsolatedStorage;
+using System.Collections;
+
+
+namespace rmDir
+{
+
+    class removeDir
+    {
+        // Process directory passed in as argument
+        public static void ProcessDirectory(string targetDirectory)
+        {
+            // Process the list of files found in the directory.
+            string[] fileEntries = Directory.GetFiles(targetDirectory);
+            foreach (string fileName in fileEntries)
+                ProcessFile(fileName);
+
+            // Recurse into subdirectories of this directory.
+            string[] subdirectoryEntries = Directory.GetDirectories(targetDirectory);
+            foreach (string subdirectory in subdirectoryEntries)
+                ProcessDirectory(subdirectory);
+        }
+
+        // Insert logic for processing found files here.
+        public static void ProcessFile(string path)
+        {
+            Console.WriteLine("Processed file '{0}'.", path);
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            String[] arguments = Environment.GetCommandLineArgs();
+            String path = arguments[1];
+
+            System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(path);
+            // Delete this dir.
+            try
+            {
+                if (Directory.Exists(path))
+                {
+                    dir.Delete(true);
+                    Console.WriteLine("\nProcess complete: \"" + path + "\" deleted");
+                }
+                else
+                {
+                    Console.WriteLine("directory not found");
+                }
+            }
+            catch (System.IO.IOException e)
+            {
+                Console.WriteLine("{0} is not a valid file or directory.", e);
+
+            }
+
+
+
+        }
+    }
+}
+
+
+
---
+++ WpfApplication1/removeFile.cs
@@ -0,0 +1,43 @@
+﻿// David Djernaes
+// CS410 Agile group project
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+
+namespace rmFile
+{
+    class removeFile
+    {
+    
+        // remove file from directory. Takes first argument from command line and 
+        // checks if valid input, deletes file from directory and outputs success message
+        static void Main(string[] args)
+        {
+            String[] arguments = Environment.GetCommandLineArgs();
+            String path = arguments[1];
+
+            System.IO.FileInfo file = new System.IO.FileInfo(path);
+            try
+            {
+                if (File.Exists(path))
+                {
+                    file.Delete();
+                    Console.WriteLine("\nProcess complete: \"" + path + "\" deleted");
+                }
+                else
+                {
+                    Console.WriteLine("file not found");
+                }
+            }
+            catch (System.IO.IOException e)
+            {
+                Console.WriteLine("{0} is not a valid file or directory.", e);
+            }
+        }
+    }
+}

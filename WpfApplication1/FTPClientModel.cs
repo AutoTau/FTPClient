929bc4fd8a8c29fd1e70202faf82904d59ce9f98
Christopher Teters <cteters@pdx.edu>
2019-07-19 12:15:41 -0700

    Added a function to display dir contents

--- WpfApplication1/FTPClientModel.cs
+++ WpfApplication1/FTPClientModel.cs
@@ -30,7 +30,8 @@ namespace WpfApplication1
             try
             {
                 string file = Path.GetFileName(FileToUpload);
-                FtpWebRequest request = (FtpWebRequest)WebRequest.Create(new Uri(string.Format($"ftp://{HostName}" + $"/{file}")));
+                FtpWebRequest request =
+                    (FtpWebRequest) WebRequest.Create(new Uri(string.Format($"ftp://{HostName}" + $"/{file}")));
                 request.Method = WebRequestMethods.Ftp.UploadFile;
                 request.Credentials = new NetworkCredential(UserName, Password);
                 Stream ftpStream = request.GetRequestStream();
@@ -38,16 +39,16 @@ namespace WpfApplication1
                 byte[] buffer = new byte[1024];
                 int byteRead = 0;
                 double read = 0;
-                double size = (double)fStream.Length;
-                
+                double size = (double) fStream.Length;
+
                 do
                 {
                     byteRead = fStream.Read(buffer, 0, 1024);
                     ftpStream.Write(buffer, 0, byteRead);
-                    read += (double)byteRead;
+                    read += (double) byteRead;
                     percentage = read / size * 100; //Progress percentage.
-                }
-                while (byteRead != 0);
+                } while (byteRead != 0);
+
                 fStream.Close();
                 ftpStream.Close();
                 return;
@@ -69,14 +70,16 @@ namespace WpfApplication1
         /// <param name="password"></param>
         /// <param name="fileToDownload"></param>
         /// <param name="port"></param>
-        public void DownloadSelectedFile(string hostName, string userName, string password, string fileToDownload, int port)
+        public void DownloadSelectedFile(string hostName, string userName, string password, string fileToDownload,
+            int port)
         {
-            var request = (FtpWebRequest)WebRequest.Create(new Uri(string.Format($"ftp://{hostName}/{fileToDownload}")));
+            var request =
+                (FtpWebRequest) WebRequest.Create(new Uri(string.Format($"ftp://{hostName}/{fileToDownload}")));
             request.Method = WebRequestMethods.Ftp.DownloadFile;
 
             request.Credentials = new NetworkCredential(userName, password);
 
-            var response = (FtpWebResponse)request.GetResponse();
+            var response = (FtpWebResponse) request.GetResponse();
             var responseStream = response.GetResponseStream();
             var reader = new StreamReader(responseStream);
             Console.WriteLine(reader.ReadToEnd());
@@ -87,6 +90,32 @@ namespace WpfApplication1
             response.Close();
         }
 
+        /// <summary>
+        /// Returns a string of file names from directory
+        /// </summary>
+        /// <param name="hostName"></param>
+        /// <param name="userName"></param>
+        /// <param name="password"></param>
+        public string GetDirectory(string hostName, string userName, string password)
+        {
+            // Get the object used to communicate with the server.
+            FtpWebRequest request = (FtpWebRequest) WebRequest.Create($"ftp://{hostName}/");
+            request.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
 
+            request.Credentials = new NetworkCredential(userName, password);
+
+            FtpWebResponse response = (FtpWebResponse) request.GetResponse();
+
+            Stream responseStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(responseStream);
+            Console.WriteLine(reader.ReadToEnd());
+
+            //Console.WriteLine($"Directory List Complete, status {response.StatusDescription}");
+            string directoryInfo = response.StatusDescription;
+
+            reader.Close();
+            response.Close();
+            return directoryInfo;
+        }
     }
 }
